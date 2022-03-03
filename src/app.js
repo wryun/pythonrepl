df51deb3529e400601620de959d1a36bb5981b7e
James Haggerty <james@gruemail.com>
2022-03-03 14:21:29 +1100

    More editor nonsense, minor cleanups

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import 'skulpt';
+import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup";
+import {python} from "@codemirror/lang-python";
 
 window.onload = () => {
-    const textConsole = document.getElementById('python-console');
+    const textConsole = document.getElementById('console');
     //window.addEventListener('click', () => textConsole.focus());
-    window.addEventListener('paste', textConsole.onPaste.bind(textConsole), true);
+    //window.addEventListener('paste', textConsole.onPaste.bind(textConsole), true);
 
     const turtle = document.getElementById('turtle-canvas');
     const repl = new PythonRepl(textConsole, turtle);
-    textConsole.focus();
+    textConsole.querySelector('text-console').focus();
     repl.run();
 };
 
@@ -19,8 +21,9 @@ shape('turtle')
 
 
 class PythonRepl {
-    constructor(textConsole, turtleCanvas) {
-        this.textConsole = textConsole;
+    constructor(console, turtleCanvas) {
+        this.console = console;
+        this.textConsole = console.querySelector('text-console');
         this.turtleCanvas = turtleCanvas;
 
         this.initSkulpt();
@@ -44,11 +47,10 @@ class PythonRepl {
             }
         });
 
-        // Defining function/class.
-        // TODO - replace with continuation exception
+        // TODO - replace with continuation exception. See changes to repl.js also.
         this.textConsole.addEditorHook((input) => {
-            if (input.trim().match(/^(def|class\s*.*:$)/)) {
-                return input.trim();
+            if (input.trim().match(/^(def|class|repeat|for|while)\s*.*:$/)) {
+                return input.trim() + '\n    ';
             }
         });
     }
@@ -88,20 +90,18 @@ class PythonRepl {
             if (res === undefined) {
                 continue
             }
-            const changes = Object.entries(Sk.globals).filter(([k, v]) => v !== oldGlobals[k]);
-            if (changes.length !== 1) {
-              // TODO - shouldn't happen.
-              // (really, need to parse AST to prevent this before we eval, and make sure k is sane)
-              return;
-            }
-            for (const [k, v] of changes) {
-              this.plainText[k] = input;
-            }
-
             if (res.$d['__last_expr_result__'] !== Sk.builtin.none.none$) {
                 this.textConsole.output(Sk.builtin.repr(res.$d['__last_expr_result__']).v + '\n');
             }
             delete res.$d['__last_expr_result__'];
+
+            const changes = Object.entries(Sk.globals).filter(([k, v]) => v !== oldGlobals[k]);
+            if (changes.length === 1) {
+              for (const [k, v] of changes) {
+                console.log('change', k, v);
+                this.plainText[k] = input;
+              }
+            }
         }
     }
 
@@ -123,7 +123,10 @@ class PythonRepl {
     }
 
     initSkulpt() {
-        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = this.turtleCanvas;
+        const tgConfig = Sk.TurtleGraphics || (Sk.TurtleGraphics = {});
+        tgConfig.target = this.turtleCanvas;
+        tgConfig.width = 0;
+        tgConfig.height = 0;
         Sk.configure({
             __future__: Sk.python3,
             output: this.textConsole.output.bind(this.textConsole),
@@ -145,20 +148,31 @@ class TextConsole extends HTMLElement {
         this.currentLine = '';
         this.oldLines = [];
 
+        this.temporaryEditorView = null;
         this.editorHooks = [];
 
         this.shadow = this.attachShadow({mode: 'open'});
-        const template = document.getElementById('consoletemplate').content.cloneNode(true)
-        this.shadow.append(template);
-        
+        this.consoleElem = document.getElementById('consoletemplate').content.cloneNode(true);
+        this.shadow.append(this.consoleElem);
+
         this.boxElem = this.shadow.getElementById('box');
+
+        // TODO doesn't work
+        EditorView.domEventHandlers({resize: (event, view) => {
+            this.parent.scrollTop = this.parent.scrollHeight;
+        }});
     }
 
     connectedCallback() {
+        // Dirty hack.
+        this.parent = this.closest('div');
+
         // set up listeners!!
         this.prompt = this.getAttribute('data-prompt');
         this.output(this.getAttribute('data-intro') + '\n');
-        this.addEventListener('keydown', this.onKeydown.bind(this), true);
+        this.addEventListener('keydown', this.onKeydown.bind(this), {capture: false});
+        this.addEventListener('paste', this.onPaste.bind(this), {capture: false});
+        this.addEventListener('focus', this.onFocus.bind(this), {capture: false});
     }
 
     addEditorHook(detectFn) {
@@ -183,6 +197,9 @@ class TextConsole extends HTMLElement {
     output(text) {
         this.boxElem.textContent = this.boxElem.textContent.slice(0, -1);
         this.boxElem.append(document.createTextNode(text + '■'));
+        if (text.includes('\n')) {
+            this.parent.scrollTop = this.parent.scrollHeight;
+        }
     }
 
     showPrompt() {
@@ -197,12 +214,20 @@ class TextConsole extends HTMLElement {
         if (lines.length > 1) {
           for (const line of lines.slice(0, -1)) {
             this.currentLine += line + '\n';
-            this.output(line + '\n');
+            this.output(line);
             this.onEnter();
           }
         }
         this.currentLine += lines[lines.length - 1];
         this.output(lines[lines.length - 1]);
+    }
+
+    onFocus(event) {
+        if (this.temporaryEditorView) {
+            event.preventDefault();
+            event.stopPropagation();
+            this.temporaryEditorView.focus();
+        }
     }
 
     onKeydown(event) {
@@ -219,7 +244,6 @@ class TextConsole extends HTMLElement {
         case 'Control':
             break;
         case 'Enter':
-            this.output('\n');
             this.onEnter();
             break;
         case 'Backspace':
@@ -245,7 +269,9 @@ class TextConsole extends HTMLElement {
               // TODO handle oldLines in buffer...
               const proposedInput = hook(this.currentLine);
               if (proposedInput) {
-                console.log('should be spawning editor');
+                this.boxElem.textContent = this.boxElem.textContent.slice(0, -(this.currentLine.length + 1));
+                this.spawnTemporaryEditor(proposedInput);
+                return;
               }
             }
             this.readlinePromiseResolve(this.currentLine);
@@ -253,7 +279,41 @@ class TextConsole extends HTMLElement {
         } else {
             this.oldLines.push(this.currentLine);
         }
+        this.output('\n');
         this.currentLine = '';
+    }
+
+    spawnTemporaryEditor(proposedInput) {
+        const view = this.temporaryEditorView = new EditorView({
+            state: EditorState.create({doc: proposedInput, extensions: [basicSetup, python()]}),
+            parent: this.parent,
+        });
+        view.focus();
+        // TODO make it go to the bottom.
+        view.moveVertically(view.state.selection.main, true);
+
+        let onEditorFinish = (text) => {
+            doneButton.remove();
+            cancelButton.remove();
+            this.temporaryEditorView.destroy();
+            this.temporaryEditorView = null;
+            // TODO - Print it out, but _only_ if it results in a change...
+            this.focus();
+            this.output('\n' + text + '\n');
+            this.readlinePromiseResolve(text);
+            this.readlinePromise = null;
+            this.currentLine = '';
+        };
+
+        const doneButton = document.createElement('button');
+        doneButton.append(document.createTextNode('Done'));
+        doneButton.onclick = () => onEditorFinish(view.state.doc.toString());
+        this.parent.append(doneButton);
+
+        const cancelButton = document.createElement('button');
+        cancelButton.append(document.createTextNode('Cancel'));
+        cancelButton.onclick = () => onEditorFinish('');
+        this.parent.append(cancelButton);
     }
 }
 
